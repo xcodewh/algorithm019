3e748b22d1a765c86a75f374fe67f0d0950969da
Ceci <xil1009@yahoo.com>
2020-11-30 01:18:04 +0000

    update week4 MyBinarySearch

--- Notes/Notes.md
+++ Notes/Notes.md
@@ -1,3 +1,14 @@
+# 作业提交格式
+
+```
+#学号: G20200343150113
+#姓名:Ceci
+#班级:19期
+#语言:Java
+#作业链接:https://github.com/xcodewh/algorithm019/tree/main/Week_04
+#总结链接:https://github.com/xcodewh/algorithm019/blob/main/Week_04/README.md
+```
+
 # Binary Search
 
 ```java
--- Week_04/MyBinarySearch.java
+++ Week_04/MyBinarySearch.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /**
  * This class uses the binary search to find the rotate position in a rotated sorted array (in ascending order).
  * 
@@ -6,17 +8,29 @@
 class MyBinarySearch {
     public int search(int[] nums) {
         int left = 0, right = nums.length -1, mid;
-        if (right < 2) return 0;
+        if (right < 1) return -1;
         while (left <= right) {
             mid = (right - left) / 2 + left;
             if (left == right -1) { //This is where the array is rotated
-                return left;
+                if ((nums[left] > nums[right])) {
+                    return left;
+                } else {
+                    return -1;
+                }
             } else if (nums[left] > nums[mid]) { //The rotate position is on the left
                 right = mid;
             } else { //The rotate position is on the right
                 left = mid;
             }
         }
-        return 0;
+        return -1;
+    }
+
+    public static void main(String[] args) throws Exception {
+        int[] nums = {4,5,6,7,0,1,2};
+        MyBinarySearch mySearch = new MyBinarySearch();
+        int pos = mySearch.search(nums);
+        System.out.println(Arrays.toString(nums)); 
+        System.out.println(pos);
     }
 }
